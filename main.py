bcd38ebc68d95692214d99571df1104b64552d4b
Pavel Urvantsev <pavelu@kth.se>
2025-05-09 00:17:44 +0200

    structure overhaul and automation

---
+++ main.py
@@ -0,0 +1,53 @@
+import json
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from wspr import wsprlive_get_info, wsprlive_pull_one_month
+from voacap import run_voacap
+
+global FROM_DATE, TO_DATE, CONFIG, SSN_DATA
+
+
+def read_config():
+    global FROM_DATE, TO_DATE, CONFIG, SSN_DATA
+
+    SSN_DATA = json.load(open('data/ssn.json'))
+    CONFIG = json.load(open('config.json'))
+    time_period = CONFIG['time_period']
+
+    FROM_DATE = datetime.strptime(time_period['from'] + "-01", "%Y-%m-%d").date()
+    TO_DATE = datetime.strptime(time_period['to'] + "-01", "%Y-%m-%d").date()
+
+
+def one_month(circuit, current_date):
+    info = wsprlive_get_info(circuit, current_date)
+    tx_lat = info['tx_lat']
+    tx_lon = info['tx_lon']
+    rx_lat = info['rx_lat']
+    rx_lon = info['rx_lon']
+
+    MONTH = current_date.strftime("%Y %m.00")
+    SSN = next((item['ssn'] for item in SSN_DATA if item['time-tag'] == current_date.strftime("%Y-%m")))
+    TX = circuit["tx"]
+    RX = circuit["rx"]
+    CIRCUIT = f"{abs(tx_lat):05.2f}{'N' if tx_lat >= 0 else 'S'}   {abs(tx_lon):06.2f}{'E' if tx_lon >= 0 else 'W'}    {abs(rx_lat):05.2f}{'N' if rx_lat >= 0 else 'S'}   {abs(rx_lon):06.2f}{'E' if rx_lon >= 0 else 'W'}"
+    NOISE = circuit['noise']
+    POWER = f"{info['power'] * 0.0008:.4f}"  # divide by 1000 and 80% efficiency, VOACAP online does that
+
+    run_voacap(MONTH, SSN, TX, RX, CIRCUIT, NOISE, POWER)
+    wsprlive_pull_one_month(TX, RX, MONTH, current_date)
+
+
+def main():
+    read_config()
+
+    for circuit in CONFIG["circuits"]:
+        circuit["noise"] = CONFIG["noise_levels"].get(circuit["noise"])  # Translate noise
+
+        current_date = FROM_DATE
+        while current_date <= TO_DATE:
+            one_month(circuit, current_date)
+            current_date += relativedelta(months=1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ wspr.py
@@ -0,0 +1,35 @@
+import urllib.request
+import json
+from pathlib import Path
+
+from dateutil.relativedelta import relativedelta
+
+bands = ["-1", "0", "1", "3", "5", "7", "10", "14", "18", "21", "24", "28", "50", "70", "144", "432", "1296"]
+
+def wsprlive_get(query):
+    url = "https://db1.wspr.live/?query=" + urllib.parse.quote_plus(query + " FORMAT JSON")
+    contents = urllib.request.urlopen(url).read()
+    return json.loads(contents.decode("UTF-8"))["data"]
+
+
+def wsprlive_get_info(circuit, current_date):
+    return wsprlive_get(
+        f"SELECT tx_lat, tx_lon, rx_lat, rx_lon, power  FROM rx WHERE tx_sign = '{circuit['tx']}' AND rx_sign = '{circuit['rx']}' AND '{current_date}' <= time AND time < '{current_date + relativedelta(months=1)}' LIMIT 1")[
+        0]
+
+
+def wsprlive_pull_one_month(TX, RX, MONTH, current_date):
+    global bands
+
+    path = Path("data") / "wspr" / f"{TX}_{RX}"
+    path.mkdir(parents=True, exist_ok=True)
+
+    for band in bands:
+        band_path = path / band
+        band_path.mkdir(parents=True, exist_ok=True)
+
+        json_obj = wsprlive_get(
+            f"SELECT time, band, frequency, snr, power FROM rx WHERE band = '{band}' AND tx_sign = '{TX}' AND rx_sign = '{RX}' AND '{current_date}' <= time AND time < '{current_date + relativedelta(months=1)}' ORDER BY time ASC")
+        with open(band_path / f"{MONTH.replace(" ", "_")}.json","w") as file:
+            json.dump(json_obj, file)
+
